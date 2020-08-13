618b66c5166e040c8e551d2390881fcdb3eec999
Subliminal_Guy <subliminal_kid@posteo.de>
2020-08-13 14:48:20 +0200

    first commit

---
+++ precache-manifest.8134274b2cd761bc560cf8b52d8903f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f16a5dc56f2ac9e62473a771007a0e2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5019c32b5597dcfc401",
+    "url": "/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "c1b6b302312f70db4d53",
+    "url": "/static/js/2.f6c5d7cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f6c5d7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5019c32b5597dcfc401",
+    "url": "/static/js/main.96014bdc.chunk.js"
+  },
+  {
+    "revision": "802b7d4df969e07a5853",
+    "url": "/static/js/runtime-main.bbc71d02.js"
+  }
+]);
